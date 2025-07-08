545adeff50b183986a2505ddaa23f1acafbda988
Chue Mi Mi Yadanar Htwe <chue.bcn@gmail.com>
2025-07-08 10:37:37 +0630

    feat: Validate Mode of Payment base on Company Curreny

--- sales_app/eactive_sales_app/doctype/sales_app_payment_entry/sales_app_payment_entry.js
+++ sales_app/eactive_sales_app/doctype/sales_app_payment_entry/sales_app_payment_entry.js
@@ -1,8 +1,25 @@
 // Copyright (c) 2025, Eactive Techonologies and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Sales App Payment Entry", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Sales App Payment Entry", {
+	refresh(frm) {
+        frm.events.set_query(frm);
+	},
+    
+    set_query(frm) {
+        if (!frm.doc.company) return;
+        frappe.call({
+            method: "get_mode_of_payment_base_on_company_currency",
+            doc: frm.doc,
+            callback: function (r) {                
+                frm.set_query('mode_of_payment', () => {
+                    return {
+                        filters: [
+                            ['Mode of Payment', 'name', 'in', r.message]
+                        ]
+                    };
+                });
+            },
+        });        
+    }      
+});
--- sales_app/eactive_sales_app/doctype/sales_app_payment_entry/sales_app_payment_entry.py
+++ sales_app/eactive_sales_app/doctype/sales_app_payment_entry/sales_app_payment_entry.py
@@ -1,9 +1,35 @@
 # Copyright (c) 2025, Eactive Techonologies and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+
 from frappe.model.document import Document
+# from erpnext.accounts.party import get_party_account
+# from erpnext.accounts.doctype.payment_entry.payment_entry import get_account_details
 
 
 class SalesAppPaymentEntry(Document):
-	pass
+	
+	@frappe.whitelist()
+	def get_mode_of_payment_base_on_company_currency(self):
+		company_currency = frappe.db.get_value("Company", self.company, "default_currency")
+		account_with_company_currency = frappe.db.get_all("Account", 
+			filters={
+				"disabled": 0,
+				"company": self.company,
+				"account_currency": company_currency
+			},
+			pluck="name"
+		)
+		valid_mode_of_payments = []
+		if account_with_company_currency:
+			valid_mode_of_payments = frappe.db.get_all("Mode of Payment Account", 
+				filters={
+					"default_account": ["In", account_with_company_currency]
+				},
+				pluck="parent"
+			)
+		return valid_mode_of_payments
+
+	
+			
